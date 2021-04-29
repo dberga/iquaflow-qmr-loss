da84520ff3ce6f0659c0d77102990445dff787d6
David Berga <david.berga@eurecat.org>
2021-04-29 11:27:01 +0200

    download satellite datasets updated links

---
+++ download_bb_datasets.sh
@@ -0,0 +1,8 @@
+wget -nc https://public-remote-sensing-datasets.s3-eu-west-1.amazonaws.com/compressed/DeepGlobe-50cm.tar.gz
+wget -nc https://public-remote-sensing-datasets.s3-eu-west-1.amazonaws.com/compressed/ECODSEdataset.zip
+wget -nc https://public-remote-sensing-datasets.s3-eu-west-1.amazonaws.com/compressed/Inria-NEW2-AerialImageDataset.zip
+wget -nc https://public-remote-sensing-datasets.s3-eu-west-1.amazonaws.com/compressed/kaggle-ships-in-satellite-imagery-3meter-pixel-size.zip
+wget -nc https://public-remote-sensing-datasets.s3-eu-west-1.amazonaws.com/compressed/UCMerced_LandUse_30cm.zip
+wget -nc https://public-remote-sensing-datasets.s3-eu-west-1.amazonaws.com/compressed/USGS.tgz
+wget -nc https://public-remote-sensing-datasets.s3-eu-west-1.amazonaws.com/compressed/xview_train_images.tgz
+wget -nc https://public-remote-sensing-datasets.s3-eu-west-1.amazonaws.com/compressed/xview_val_images.tgz
