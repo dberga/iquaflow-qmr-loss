24919504ef34cf1c1bc6201cb214e961f45c156f
David Berga <david.berga@eurecat.org>
2021-11-08 23:01:05 +0100

    2>&1 processes

--- MSRN_EXAMPLE/iqf_uab/train.sh
+++ MSRN_EXAMPLE/iqf_uab/train.sh
@@ -1,15 +1,15 @@
-nohup python3 training.py \
+python3 training.py \
 	--trainid "vanilla_20epoch_crops64_bsize16_lr0001" \
 	--nEpochs 20 \
 	--crop_size 64 \
 	--batchSize 16 \
 	--lr 0.001 \
-	--cuda --gpus "0" > "log_1.txt" & 
+	--cuda --nockpt --gpus "0" > "log_1.txt" 2>&1 ; 
 	#--resume \
 	#--add_noise \
 	#--vgg_loss \
 	#--regressor_loss "sigma" \ 
-	#--regressor_criterion="BCELoss_mean" \ 
+	#--regressor_criterion "BCELoss_mean" \ 
 	#--regressor_loss_factor=1.0 \
 	#--regressor_gt2onehot \
 	#--regressor_onorm \
@@ -18,77 +18,77 @@ nohup python3 training.py \
 	#--trainds_input test_datasets/AerialImageDataset/train/images
 	#--valds_input test_datasets/AerialImageDataset/test/images
 	#--step 50
-	
-nohup python3 training.py \
+: '	
+python3 training.py \
 	--trainid "sigma_MSEloss_20epoch_crops64_bsize16_lr0001_factor010_clamp" \
 	--nEpochs 20 \
 	--crop_size 64 \
 	--batchSize 16 \
 	--lr 0.001 \
-	--regressor_loss "sigma" \ 
-	--regressor_criterion="MSELoss_mean" \ 
-	--regressor_loss_factor=0.10 \
+	--regressor_loss "sigma" \
+	--regressor_criterion "MSELoss_mean" \
+	--regressor_loss_factor 0.10 \
 	--regressor_zeroclamp \
-	--cuda --gpus "0" > "log_2.txt" & 
-
-nohup python3 training.py \
+	--cuda --nockpt --gpus "0" > "log_2.txt" 2>&1 ;
+'
+python3 training.py \
 	--trainid "snr_MSEloss_20epoch_crops64_bsize16_lr0001_factor010_clamp" \
 	--nEpochs 20 \
 	--crop_size 64 \
 	--batchSize 16 \
 	--lr 0.001 \
-	--regressor_loss "snr" \ 
-	--regressor_criterion="MSELoss_mean" \ 
-	--regressor_loss_factor=0.10 \
+	--regressor_loss "snr" \
+	--regressor_criterion "MSELoss_mean" \
+	--regressor_loss_factor 0.10 \
 	--regressor_zeroclamp \
-	--cuda --gpus "0" > "log_3.txt" & 
+	--cuda --nockpt --gpus "0" > "log_3.txt" 2>&1 ;
 
-nohup python3 training.py \
+python3 training.py \
 	--trainid "sigma_BCEloss_20epoch_crops64_bsize16_lr0001_gt2onehot_factor010" \
 	--nEpochs 20 \
 	--crop_size 64 \
 	--batchSize 16 \
 	--lr 0.001 \
-	--regressor_loss "sigma" \ 
-	--regressor_criterion="BCELoss_mean" \ 
-	--regressor_loss_factor=0.10 \
+	--regressor_loss "sigma" \
+	--regressor_criterion "BCELoss_mean" \
+	--regressor_loss_factor 0.10 \
 	--regressor_gt2onehot \
-	--cuda --gpus "0" > "log_4.txt" & 
+	--cuda --nockpt --gpus "0" > "log_4.txt" 2>&1 ; 
 
-nohup python3 training.py \
+python3 training.py \
 	--trainid "snr_BCEloss_20epoch_crops64_bsize16_lr0001_gt2onehot_factor010" \
 	--nEpochs 20 \
 	--crop_size 64 \
 	--batchSize 16 \
 	--lr 0.001 \
-	--regressor_loss "snr" \ 
-	--regressor_criterion="BCELoss_mean" \ 
-	--regressor_loss_factor=0.10 \
+	--regressor_loss "snr" \
+	--regressor_criterion "BCELoss_mean" \
+	--regressor_loss_factor 0.10 \
 	--regressor_gt2onehot \
-	--cuda --gpus "0" > "log_5.txt" & 
+	--cuda --nockpt --gpus "0" > "log_5.txt" 2>&1 ; 
 
-
-nohup python3 training.py \
+: '
+python3 training.py \
 	--trainid "sigma_L1loss_20epoch_crops64_bsize16_lr0001_factor010_clamp" \
 	--nEpochs 20 \
 	--crop_size 64 \
 	--batchSize 16 \
 	--lr 0.001 \
-	--regressor_loss "sigma" \ 
-	--regressor_criterion="L1Loss_mean" \ 
-	--regressor_loss_factor=0.10 \
+	--regressor_loss "sigma" \
+	--regressor_criterion "L1Loss_mean" \
+	--regressor_loss_factor 0.10 \
 	--regressor_zeroclamp \
-	--cuda --gpus "0" > "log_6.txt" & 
-
-nohup python3 training.py \
+	--cuda --nockpt --gpus "0" > "log_6.txt" 2>&1 ; 
+'
+python3 training.py \
 	--trainid "snr_L1loss_20epoch_crops64_bsize16_lr0001_factor010_clamp" \
 	--nEpochs 20 \
 	--crop_size 64 \
 	--batchSize 16 \
 	--lr 0.001 \
-	--regressor_loss "sigma" \ 
-	--regressor_criterion="L1Loss_mean" \ 
-	--regressor_loss_factor=0.10 \
+	--regressor_loss "sigma" \
+	--regressor_criterion "L1Loss_mean" \
+	--regressor_loss_factor 0.10 \
 	--regressor_zeroclamp \
-	--cuda --gpus "0" > "log_6.txt" & 
+	--cuda --nockpt --gpus "0" > "log_7.txt" 2>&1 ; 
 
