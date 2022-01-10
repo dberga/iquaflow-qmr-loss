f0776bcf62daa710c019f972fb0257f9889ca9b8
David Berga <david.berga@eurecat.org>
2022-01-10 16:00:07 +0000

    updated tb2csv stacking all experiment benchmark

--- MSRN_EXAMPLE/iqf_uab/tb2csv.py
+++ MSRN_EXAMPLE/iqf_uab/tb2csv.py
@@ -33,6 +33,7 @@ def event2arrays(experiment_path):
 def events2csv(dirpath = "./msrn/experiment"):
 	experiment_paths=glob.glob(os.path.join(dirpath,'*'))
 	for experiment_path in experiment_paths:
+		print("Parsing tfevents from "+experiment_path)
 		if len(glob.glob(os.path.join(experiment_path,'events.out.tfevents*'))):
 			experiment_name=os.path.dirname(experiment_path)
 			all_vals = event2arrays(experiment_path)
@@ -43,6 +44,55 @@ def events2csv(dirpath = "./msrn/experiment"):
 		        fmt='%s'
 		    )
 		else:
-			print(experiment_path + "has no tfevents")
-#def events2metriccsv(dirpath = "./msrn/experiment"):
-# to do: stack all experiments for each metric separately (one csv per metric, all experiments in row)
+			print(experiment_path + " has no tfevents")
+
+def events2csv_stack(dirpath = "./msrn/experiment"):
+	experiment_paths=glob.glob(os.path.join(dirpath,'*'))
+	stacked_vals = []
+	stacked_metrics = []
+	empty_token=0
+	# read tfevents to a list
+	for experiment_path in experiment_paths:
+		print("Parsing tfevents from "+experiment_path)
+		if len(glob.glob(os.path.join(experiment_path,'events.out.tfevents*'))):
+			experiment_name=os.path.dirname(experiment_path)
+			all_vals = event2arrays(experiment_path)
+			all_vals_dict = {}
+			for metric in all_vals:
+				metric_name = metric[0].replace("/","_")
+				all_vals_dict[metric_name]=metric[1:]
+				stacked_metrics.append(metric_name)
+			stacked_vals.append(all_vals_dict)
+		else:
+			print(experiment_path + " has no tfevents")
+	stacked_metrics=list(set(stacked_metrics))
+
+	for metric in stacked_metrics:
+		all_rows = []
+		# stack experiment values per metric
+		for i,all_vals in enumerate(stacked_vals):
+			experiment_path = experiment_paths[i]
+			if len(glob.glob(os.path.join(experiment_path,'events.out.tfevents*'))):
+				experiment_name=os.path.basename(experiment_path)
+				if metric in all_vals.keys():
+					metric_row=all_vals[metric]
+					metric_row.insert(0,experiment_name)
+					all_rows.append(metric_row)		
+		if not len(all_rows):
+			break
+		# add empty values to have same cols for each metric (some experiments have more iters/epochs)
+		maxlen=len(max(all_rows,key=len))
+		for r,row in enumerate(all_rows):
+			lendiff=maxlen-len(all_rows[r])
+			if lendiff>0:
+				for col in range(lendiff):
+					all_rows[r].append(empty_token)
+		np.savetxt(
+	        os.path.join(dirpath, metric+".csv"),
+	        np.asarray(all_rows),
+	        delimiter=",",
+	        fmt='%s'
+	    )
+if __name__ == "__main__":
+    # events2csv(dirpath = "./msrn/experiment")
+    events2csv_stack(dirpath = "./msrn/experiment")
