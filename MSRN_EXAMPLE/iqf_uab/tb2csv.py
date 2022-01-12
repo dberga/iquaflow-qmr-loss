708fc751b592d979392130ab9d21c80b50394267
David Berga <david.berga@eurecat.org>
2022-01-12 17:53:12 +0000

    fix plots size and kept unrounded values

--- MSRN_EXAMPLE/iqf_uab/tb2csv.py
+++ MSRN_EXAMPLE/iqf_uab/tb2csv.py
@@ -5,10 +5,13 @@ import glob
 import os
 
 
-def formatvals(vals, round_decimals = 5):
+def formatvals(vals, round_decimals = 5, tostr=True):
 	newvals = []
 	for val in vals:
-		newval=str(round(val,round_decimals))
+		if tostr:
+			newval=str(round(val,round_decimals))
+		else:
+			newval=round(val,round_decimals)
 		newvals.append(newval)
 	return newvals
 
@@ -22,21 +25,24 @@ def event2arrays(experiment_path):
 	
 	# read and stack scalar values
 	all_vals=[]
+	all_vals_num=[]
 	for s in scalars:  # wall clock, number of steps and value for a scalar
 		clock, steps, vals = zip(*event_acc.Scalars(s))
-		vals = formatvals(vals)
+		vals_num = vals
+		vals = formatvals(vals,True)
 		vals.insert(0,s) # scalar name at first row
 		all_vals.append(vals)
+		all_vals_num.append(vals_num)
 	# to do: histograms
 
-	return all_vals #all stacked values
+	return all_vals, all_vals_num #all stacked values
 		
 def events2csv(dirpath = "./msrn/experiment"):
-	experiment_paths=glob.glob(os.path.join(dirpath,'*'))
+	experiment_paths=glob.glob(os.path.join(dirpath,'*/'))
 	for experiment_path in experiment_paths:
 		print("Parsing tfevents from "+experiment_path)
 		if len(glob.glob(os.path.join(experiment_path,'events.out.tfevents*'))):
-			experiment_name=os.path.dirname(experiment_path)
+			experiment_name=os.path.basename(os.path.dirname(experiment_path))
 			all_vals = event2arrays(experiment_path)
 			np.savetxt(
 		        os.path.join(experiment_path, "events.csv"),
@@ -48,26 +54,29 @@ def events2csv(dirpath = "./msrn/experiment"):
 			print(experiment_path + " has no tfevents")
 
 def events2csv_stack(dirpath = "./msrn/experiment", plot=True):
-	experiment_paths=glob.glob(os.path.join(dirpath,'*'))
+	experiment_paths=glob.glob(os.path.join(dirpath,'*/'))
 	stacked_vals = []
+	stacked_vals_num = []
 	stacked_metrics = []
 	empty_token=0
 	# read tfevents to a list
 	for experiment_path in experiment_paths:
 		print("Parsing tfevents from "+experiment_path)
 		if len(glob.glob(os.path.join(experiment_path,'events.out.tfevents*'))):
-			experiment_name=os.path.dirname(experiment_path)
-			all_vals = event2arrays(experiment_path)
+			experiment_name=os.path.basename(os.path.dirname(experiment_path))
+			all_vals,all_vals_num = event2arrays(experiment_path)
 			all_vals_dict = {}
-			for metric in all_vals:
+			all_vals_num_dict = {}
+			for m,metric in enumerate(all_vals):
 				metric_name = metric[0].replace("/","_")
 				all_vals_dict[metric_name]=metric[1:]
+				all_vals_num_dict[metric_name]=all_vals_num[m]
 				stacked_metrics.append(metric_name)
 			stacked_vals.append(all_vals_dict)
+			stacked_vals_num.append(all_vals_num_dict)
 		else:
 			print(experiment_path + " has no tfevents")
 	stacked_metrics=list(set(stacked_metrics))
-
 	for metric in stacked_metrics:
 		all_rows = []
 		all_rows_data = []
@@ -76,13 +85,13 @@ def events2csv_stack(dirpath = "./msrn/experiment", plot=True):
 		for i,all_vals in enumerate(stacked_vals):
 			experiment_path = experiment_paths[i]
 			if len(glob.glob(os.path.join(experiment_path,'events.out.tfevents*'))):
-				experiment_name=os.path.basename(experiment_path)
+				experiment_name=os.path.basename(os.path.dirname(experiment_path))
 				if metric in all_vals.keys():
 					metric_row=all_vals[metric]
-					all_rows_data.append(metric_row)
-					metric_row.insert(0,experiment_name)
-					all_rows.append(metric_row)		
 					all_rows_tags.append(experiment_name)
+					metric_row.insert(0,experiment_name)
+					all_rows.append(metric_row)
+					all_rows_data.append(stacked_vals_num[i][metric]) 
 		if not len(all_rows):
 			break
 		# add empty values to have same cols for each metric (some experiments have more iters/epochs)
@@ -92,6 +101,7 @@ def events2csv_stack(dirpath = "./msrn/experiment", plot=True):
 			if lendiff>0:
 				for col in range(lendiff):
 					all_rows[r].append(empty_token)
+		print("Generating "+os.path.join(dirpath, metric+".csv"))
 		np.savetxt(
 	        os.path.join(dirpath, metric+".csv"),
 	        np.asarray(all_rows),
@@ -99,7 +109,8 @@ def events2csv_stack(dirpath = "./msrn/experiment", plot=True):
 	        fmt='%s'
 	    )
 		if plot:
-			plot_1d(all_rows_data, metric, dirpath, ["iter", metric], all_rows_tags)
+			print("Plotting "+os.path.join(dirpath, metric+".png"))
+			plot_1d(all_rows_data, metric, dirpath, ["iter", metric], all_rows_tags, (10,10))
 if __name__ == "__main__":
     # events2csv(dirpath = "./msrn/experiment")
-    events2csv_stack(dirpath = "./msrn/experiment", True)
+    events2csv_stack(dirpath = "./msrn/experiment", plot=True)
